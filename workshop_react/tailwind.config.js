f68151abd11d79ef895a73d630ac0737096a0383
Saad AKLLAM <63886515+EVBLOOD@users.noreply.github.com>
2023-10-05 13:02:03 +0100

    responsive done - merge with the main to implement the navbar-

--- workshop_react/tailwind.config.js
+++ workshop_react/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'evmd': { 'max': '1300px' },
+      // => @media (max-width: 1300px) { ... }
+
+      'evsm': { 'max': '810px' },
+      // => @media (max-width: 810px) { ... }
+    },
     extend: {
+
     },
   },
   plugins: [],
