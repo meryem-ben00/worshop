3d2aeb0ba742f5765ce95276c85800846161ed15
younelma <youne.elma@gmail.com>
2023-10-12 14:44:04 +0100

    Update the patients page and add some components

--- workshop_react/src/pages/patients/index.jsx
+++ workshop_react/src/pages/patients/index.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import NavBar from "../../Views/Navbar/NavBar";
-import "./registerPatient.css";
+import Pagination from "../../components/Pagination";
+import UserPatients from "./userPatients";
+import "./patients.css";
+import TitleComponent from "../../components/TitleComponent";
+import data from "./dataTest";
 
 const RegisterPatient = () => {
   return (
     <>
       <NavBar />
-      <div className="container mx-auto  mt-20 ">
-        <Comp1 />
-        <Patients data={data} />
+      <div className="container mx-auto mt-10">
+        <TitleComponent />
+        <UserPatients data={data} />
         <Pagination />
       </div>
     </>
